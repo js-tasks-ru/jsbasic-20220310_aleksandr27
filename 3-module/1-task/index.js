f3596f1c88cbbd3531a83cd87b32859796ef960a
Aleksandr Zuev <artminor@yandex.ru>
2022-03-22 23:55:58 +0300

    3 module 1 task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,17 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
 function namify(users) {
-  // ваш код...
+  let arr = [];
+  
+  for (let key in users) {
+    arr.push(users[key].name)
+  }
+  
+  return arr
 }
+
+let names = namify(users);
