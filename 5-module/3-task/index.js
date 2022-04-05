d406f19f5df4c740094eec66fb7a2e49ded880b4
Aleksandr Zuev <artminor@yandex.ru>
2022-04-05 15:46:39 +0400

    module-5 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector(".carousel__arrow_right");
+  let carouselArrowLeft = document.querySelector(".carousel__arrow_left");
+  let carouselInner = document.querySelector(".carousel__inner");
+  let carouselSlide = document.querySelector(".carousel__slide");
+  let carouselImg = document.querySelectorAll(".carousel__img");
+  let offsetWidth = carouselSlide.offsetWidth;
+  let offset = 0;
+  let offsetMax = (carouselImg.length - 1) * offsetWidth ;
+
+  carouselArrowLeft.style.display = "none";
+
+  function arrowsHideShow() {
+    if (offsetMax <= offset) {
+      carouselArrowRight.style.display = "none";
+    } else if (offsetMax >= offset) {
+      carouselArrowRight.style.display = "flex";
+    }
+
+    if ( offset <= 0){
+      carouselArrowLeft.style.display = "none";
+    } else if (offsetMax >= offset) {
+      carouselArrowLeft.style.display = "flex";
+    }
+  }
+
+  carouselArrowRight.addEventListener("click", function() {
+    offset += offsetWidth;
+    carouselInner.style.transform = `translateX(${-offset}px)`;
+    arrowsHideShow()
+  });
+
+  carouselArrowLeft.addEventListener("click", function() {
+    offset -= offsetWidth;
+    carouselInner.style.transform = `translateX(${-offset}px)`;
+    arrowsHideShow()
+  });
+
 }
