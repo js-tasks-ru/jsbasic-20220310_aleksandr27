47d67a6bad45ced7e596416bd38904cfc92ec612
Aleksandr Zuev <artminor@yandex.ru>
2022-04-27 23:37:45 +0400

    module-9

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,7 +1,9 @@
 export default function promiseClick(button) {
+
   return new Promise((resolve, reject) => {
     button.addEventListener('click', (event) => {
-      resolve(event);
+      return resolve(event);
     }, { once: true });
   });
+
 }
